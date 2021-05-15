e8d20c8c79344937478a4ba6d8c9474e0b88d3e4
Amanda Tell <amanda@tengai-unbiased.com>
2021-05-15 16:34:58 +0200

    change await times and refactor

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ let inGame = false
 let start = false
 
 client.on('message', msg => {
+    const awaitTime = async (msg, value = false, time = 2000) => {
+        await sleep(time);
+        if (value)
+            msg.channel.send(value)
+    }
     if (msg.content === '-help') {
         msg.channel.send('**HIGHER OR LOWER Games Rules**\nThe rules are simple. Guess if the next card presented is higher or lower. If you are wrong you have to drink as many times as cards there have been since last time someone got it wrong.\nCommands are:\n**-new game** to start a new game\n**-join** to join the game\n**-start** to start the game once everyones joined\n**-shuffle** to shuffle a new deck of cards\n**-higher/-lower** to guess if the next card is higher or lower\n**-quit** to quit the game')
     }
     else if (msg.content === '-new game') {
+        participants = []
         msg.channel.send('New game started! Type -join to join in, once you are ready type -start!')
         inGame = true
     }
@@ -45,11 +51,7 @@ client.on('message', msg => {
                 drawCard().then(res => {
                     currCard = res.data.cards[0]
                     msg.channel.send(`All shuffled! The first card of the deck if ${currCard.value} of ${currCard.suit}`, { files: [currCard.image] })
-                    const awaitTime = async (msg) => {
-                        await sleep(2000);
-                        msg.channel.send(`${participants[index]}, you turn! If you think the next card is higher type -higher else type -lower.`)
-                    }
-                    awaitTime(msg)
+                    awaitTime(msg, `${participants[index]}, you turn! If you think the next card is higher type -higher else type -lower.`, 1500)
                 })
             })
             isShuffled = true
@@ -57,6 +59,10 @@ client.on('message', msg => {
 
     } else if (msg.content === '-higher' || msg.content === '-lower') {
         if (inGame && start) {
+            if (`<@${msg.author.id}>` != participants[index]) {
+                msg.reply('wait for your turn!')
+                return
+            }
             drawCard().then(res => {
                 msg.reply(`you drew a ${res.data.cards[0].value} of ${res.data.cards[0].suit}`, { files: [res.data.cards[0].image] })
                 if (res.data.remaining === 0) {
@@ -64,37 +70,29 @@ client.on('message', msg => {
                     isShuffled = false
                 }
                 amount++
-                const awaitTime = async (msg, value = false) => {
-                    await sleep(2000);
-                    if (value)
-                        msg.channel.send(value)
-                }
                 awaitTime(msg)
                 if (getRealValue(res.data.cards[0].value) < getRealValue(currCard.value) && msg.content === '-higher') {
-                    awaitTime(msg, `${participants[index]}, you were wrong, it was LOWER! DRINK ${amount} times!`)
+                    awaitTime(msg, `${participants[index]}, you were wrong, it was LOWER! DRINK ${amount} times!`, 1000)
                     amount = 0
                 } else if (getRealValue(res.data.cards[0].value) > getRealValue(currCard.value) && msg.content === '-lower') {
-                    awaitTime(msg, `${participants[index]}, you were wrong, it was HIGHER! DRINK ${amount} times!`)
+                    awaitTime(msg, `${participants[index]}, you were wrong, it was HIGHER! DRINK ${amount} times!`, 1000)
                     amount = 0
                 } else if (getRealValue(res.data.cards[0].value) === getRealValue(currCard.value)) {
-                    awaitTime(msg, `OH NO, ${participants[index]}! You have to down your drink.`)
+                    awaitTime(msg, value = `OH NO, ${participants[index]}! You have to down your drink.`, 1000)
                     amount = 0
                 }
                 else
-                    awaitTime(msg, `${participants[index]}, CORRECT! No drinks for you.`)
+                    awaitTime(msg, `${participants[index]}, CORRECT! No drinks for you.`, 1000)
 
                 if (index + 1 === participants.length) index = 0
                 else index++
-                const awaitNextCard = async (msg) => {
-                    await sleep(3000);
-                    msg.channel.send(`${participants[index]}, you turn! If you think the next card is higher type -higher, else type -lower.`)
-                }
-                awaitNextCard(msg)
+
+                awaitTime(msg, `${participants[index]}, you turn! If you think the next card is higher type -higher, else type -lower.`, 2000)
                 currCard = res.data.cards[0]
             })
         }
         else if (!inGame) msg.channel.send(`You are not playing a game! Type -new game to start a game.`)
-        else if (!start) msg.channel.send(`You haven´t started the game yet! Type -start to start a game.`)
+        else if (!start) msg.channel.send(`You haven't started the game yet! Type -start to start a game.`)
         else if (!isShuffled) msg.channel.send(`You need to shuffle the cards to continue the game. Type -shuffle.`)
     } else if (msg.content === '-join') {
         if (!participants.includes(`<@${msg.member.user.id}>`)) {
@@ -105,7 +103,7 @@ client.on('message', msg => {
     } else if (msg.content === '-start') {
         if (participants.length < 1) msg.channel.send('At least two people needs to join to play!')
         else {
-            msg.channel.send("Let´s play! First let's shuffle the deck of cards! Type -shuffle")
+            msg.channel.send("Let's play! First let's shuffle the deck of cards! Type -shuffle")
             start = true
         }
     }
